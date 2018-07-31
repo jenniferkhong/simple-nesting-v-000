917ed8ccb91e0063da375710144921bf314e8ff4
Jennifer Hong <jennifer.hong13@gmail.com>
2018-07-31 20:03:19 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -60,12 +60,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -81,8 +75,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-
+     programmer_hash[:yukihiro_matsumoto] = {
+       :known_for => "Ruby",
+       :languages => ["LISP", "C"]
+     }
+programmer_hash
 end
 
 def changing_alan
